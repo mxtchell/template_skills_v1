350e6f82acac7d4cd3a1d2291f13027b2c6d1822
Crawford <crawford.nakayama@answerrocket.com>
2025-04-21 09:15:45 -0500

    add param for bridge chart viz and bump ar-analytics version

--- dimension_breakout.py
+++ dimension_breakout.py
@@ -84,6 +84,12 @@ logger = logging.getLogger(__name__)
             parameter_type="visualization",
             description="Table Viz Layout",
             default_value=default_table_layout
+        ),
+        SkillParameter(
+            name="bridge_chart_viz_layout",
+            parameter_type="visualization",
+            description="Bridge Chart Viz Layout",
+            default_value=default_bridge_chart_viz
         )
     ]
 )
@@ -115,7 +121,8 @@ def simple_breakout(parameters: SkillInput):
                                                             env.ba.footnotes,
                                                             parameters.arguments.max_prompt,
                                                             parameters.arguments.insight_prompt,
-                                                            parameters.arguments.table_viz_layout)
+                                                            parameters.arguments.table_viz_layout,
+                                                            parameters.arguments.bridge_chart_viz_layout)
 
     return SkillOutput(
         final_prompt=final_prompt,
@@ -135,7 +142,7 @@ def find_footnote(footnotes, df):
             break
     return dim_note
 
-def render_layout(tables, bridge_chart_data, title, subtitle, insights_dfs, warnings, footnotes, max_prompt, insight_prompt, viz_layout):
+def render_layout(tables, bridge_chart_data, title, subtitle, insights_dfs, warnings, footnotes, max_prompt, insight_prompt, viz_layout, bridge_chart_viz_layout):
     facts = []
     for i_df in insights_dfs:
         facts.append(i_df.to_dict(orient='records'))
@@ -169,7 +176,7 @@ def render_layout(tables, bridge_chart_data, title, subtitle, insights_dfs, warn
 
     if bridge_chart_data is not None:
         table_vars["bridge_data"] = [{ "data": bridge_chart_data.to_dict(orient="records") }] if bridge_chart_data is not None else []
-        rendered = wire_layout( json.loads(default_bridge_chart_viz), {**general_vars, **table_vars})
+        rendered = wire_layout( json.loads(bridge_chart_viz_layout), {**general_vars, **table_vars})
         viz_list.append(SkillVisualization(title=name, layout=rendered))
 
     return viz_list, insights, max_response_prompt, export_data
